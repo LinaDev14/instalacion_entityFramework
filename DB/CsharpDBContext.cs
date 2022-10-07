9815b189191b213465a25d98a4813064b67c0971
Linadev14 <perfilgitlinamaria@gmail.com>
2022-10-07 12:31:48 -0500

    se agregan Nugets al proyecto

---
+++ DB/CsharpDBContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DB
+{
+    public partial class CsharpDBContext : DbContext
+    {
+        public CsharpDBContext()
+        {
+        }
+
+        public CsharpDBContext(DbContextOptions<CsharpDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Beer> Beers { get; set; } = null!;
+        public virtual DbSet<Brand> Brands { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DDC-CO-LGUERRE; Database=CsharpDB; Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Beer>(entity =>
+            {
+                entity.ToTable("Beer");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.BrandId).HasColumnName("BrandID");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Brand)
+                    .WithMany(p => p.Beers)
+                    .HasForeignKey(d => d.BrandId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Beer_Brand");
+            });
+
+            modelBuilder.Entity<Brand>(entity =>
+            {
+                entity.ToTable("Brand");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
